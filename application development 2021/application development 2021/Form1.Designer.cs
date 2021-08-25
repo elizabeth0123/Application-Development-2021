b0371e6455ca85ca28924ff38905f110ebeb3ff7
elizabeth0123 <78698500+elizabeth0123@users.noreply.github.com>
2021-08-25 13:56:04 +0100

    Added buttons and labels for user selection. Front Design done.

--- application development 2021/application development 2021/Form1.Designer.cs
+++ application development 2021/application development 2021/Form1.Designer.cs
@@ -29,13 +29,72 @@ namespace application_development_2021
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(70, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 138);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sorting Algorithms";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(506, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(259, 138);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Searching Algorithms";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(51, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(714, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome, we are going to be learning about algorithms!";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(195, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(441, 27);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Please select the type of algorithm you would like to learn about.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(871, 480);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- application development 2021/application development 2021/Program.cs
+++ application development 2021/application development 2021/Program.cs
@@ -19,5 +19,6 @@ namespace application_development_2021
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+       
     }
 }
